726fa32bad2ba6794918c4a2e834e4c59f04adfb
Nikita Grinyov <nikita3grynov@gmail.com>
2025-11-09 20:35:23 +0200

    project structure

--- app/cafe.py
+++ app/cafe.py
@@ -1,6 +1,8 @@
 import datetime
 
-from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 class Cafe:
@@ -11,9 +13,11 @@ class Cafe:
         if visitor.get("vaccine") is None:
             raise NotVaccinatedError
         else:
-            if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            if (visitor["vaccine"]["expiration_date"]
+                    < datetime.date.today()):
                 raise OutdatedVaccineError
-        if visitor.get("wearing_a_mask") is None or not visitor["wearing_a_mask"]:
+        if (visitor.get("wearing_a_mask") is None
+                or not visitor["wearing_a_mask"]):
             raise NotWearingMaskError
         else:
             return f"Welcome to {self.name}"
