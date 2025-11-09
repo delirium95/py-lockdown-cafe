1a4780bd76ce17af49e0ef5b12187e9f7665a87a
Nikita Grinyov <nikita3grynov@gmail.com>
2025-11-09 20:39:30 +0200

    errors

--- app/errors.py
+++ app/errors.py
@@ -1,14 +1,18 @@
 class VaccineError(Exception):
-    pass
+    def __init__(self, message="Vaccine error"):
+        super().__init__(message)
 
 
 class NotVaccinatedError(VaccineError):
-    pass
+    def __init__(self, message="Visitor is not vaccinated"):
+        super().__init__(message)
 
 
 class OutdatedVaccineError(VaccineError):
-    pass
+    def __init__(self, message="Vaccine is expired"):
+        super().__init__(message)
 
 
 class NotWearingMaskError(Exception):
-    pass
+    def __init__(self, message="Visitor is not wearing a mask"):
+        super().__init__(message)
